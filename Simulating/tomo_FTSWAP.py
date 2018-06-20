51c96b2b1622c4162d633e42312266b0381d24be
jarndejong <38500515+jarndejong@users.noreply.github.com>
2018-06-20 23:15:34 +0200

    updates updates

--- Simulating/tomo_FTSWAP.py
+++ Simulating/tomo_FTSWAP.py
@@ -4,7 +4,7 @@ Created on Wed Jun 20 15:10:44 2018
 
 @author: Jarnd
 """
-
+import time
 import sys
 sys.path.append('../Circuits/BitwiseFTSWAP')
 sys.path.append('../Functions')
@@ -15,13 +15,19 @@ import simulationfunctions as smf
 
 import itertools as itt
 
-Ps = ['X','Y','Z','I']
+
+
+Ps = ['Z']
+Ms = ['X','Y','Z','I']
 k = 0
+start_time = time.time()
 for iteration in itt.product(Ps, repeat = nq):
     if iteration == ('I',)*nq: continue
     for phase_iteration in itt.product([0,2], repeat = nq):
-        for meas_iteration in itt.product(Ps, repeat = nq):
-            print('initial state',list(iteration))
+        for meas_iteration in itt.product(Ms, repeat = nq):
+            #print('initial state',list(iteration))
             measurements = smf.run_one_round(nq,iteration,phase_iteration,gates,p_m,p_s,list(meas_iteration))
             k+=1
-    
+
+stop_time = time.time()
+print(stop_time - start_time)
